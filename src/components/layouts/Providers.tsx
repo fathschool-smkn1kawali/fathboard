442c044236a2d7d4acb40de540987af1c1fedede
webfathforce <134572236+webfathforce@users.noreply.github.com>
2025-01-21 10:02:36 +0700

    fix ✨

--- src/components/layouts/Providers.tsx
+++ src/components/layouts/Providers.tsx
@@ -4,21 +4,21 @@ import { NextUIProvider } from "@nextui-org/react";
 import { QueryClient, QueryClientProvider } from "react-query";
 
 export const Providers: React.FC<{ children: React.ReactNode }> = ({children}) => {
-
-  const query = new QueryClient(
-    {
-      defaultOptions: {
-        queries: {
-          refetchOnWindowFocus: true,
-          refetchInterval: 50000,
-          // refetchOnMount: false,
-        },
+  
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        refetchOnWindowFocus: true,  // Mengambil data ulang ketika jendela fokus
+        refetchInterval: 5000,       // Polling setiap 5 detik
+        refetchOnMount: true,        // Mengambil data setiap kali komponen mount
+        // staleTime: 0,                // Data dianggap sudah usang setelah 0 detik
       },
-    }
-  );
+    },
+  });
+  
 
   return (
-    <QueryClientProvider client={query}>
+    <QueryClientProvider client={queryClient}>
       <NextUIProvider>{children}</NextUIProvider>
     </QueryClientProvider>
   );
